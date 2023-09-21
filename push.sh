802ca9e822ffcf12c3d28f8ebd2096091071641a
natanfudge <natan.lifsiz@gmail.com>
2023-09-21 16:53:22 +0300

    test

--- push.sh
+++ push.sh
@@ -2,4 +2,5 @@
 git commit -am "$1"
 git push
 #It will always push to main which is not optimal but works for now
-git submodule foreach 'git commit -am "$1" && git push origin HEAD:main'
+git submodule foreach git commit -am "$1"
+git submodule foreach git push origin HEAD:main
