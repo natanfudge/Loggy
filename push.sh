b4676c955c2797502092a3696c62e1bda3aa22f6
natanfudge <natan.lifsiz@gmail.com>
2023-09-21 15:51:11 +0300

    update

--- push.sh
+++ push.sh
@@ -1,7 +1,6 @@
 #!/bin/bash
-message=$1
 git add -A
-git commit -m $message
+git commit -m "$1"
 git push
 #It will always push to main which is not optimal but works for now
-git submodule foreach git add -A && git commit -m $message && git push origin HEAD:main
+git submodule foreach git add -A && git commit -m "$1" && git push origin HEAD:main
