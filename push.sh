068eda261118eecbed2082af2fde69fabc0eca3c
natanfudge <natan.lifsiz@gmail.com>
2023-09-21 16:46:29 +0300

    add utils from loggy

--- push.sh
+++ push.sh
@@ -3,4 +3,4 @@ git add -A
 git commit -m "$1"
 git push
 #It will always push to main which is not optimal but works for now
-git submodule foreach git add . && git commit -m "$1" && git push origin HEAD:main
+git submodule foreach git commit -a -m "$1" && git push origin HEAD:main
